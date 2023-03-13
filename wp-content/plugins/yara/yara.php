c340c38ed4c5bd26d6f00cdb8c0a5dbf4614f1d4
Vicho Vichev <vicho.vichev@ns-mediagroup.com>
2023-03-13 17:20:34 +0200

    final scrap data function

--- wp-content/plugins/yara/yara.php
+++ wp-content/plugins/yara/yara.php
@@ -12,8 +12,6 @@
  * @package YaraPlugin
  */
 
-include('wp-load.php');
-include_once(ABSPATH . '/wp-admin/includes/image.php');
 
 function yara_code_challenge_setup_menu()
 {
@@ -30,13 +28,6 @@ function yara_init()
         echo "You need to install woocomerce!";
     }
 }
-
-
-
-
-
-
-
 
 //FIX - 1 // Images can have more metadata for ALT and <CAPTION> !!!
 function bg_image_upload($url, $productTitle)
@@ -61,6 +52,7 @@ function bg_image_upload($url, $productTitle)
             'test_form' => false,
             'test_size' => true,
         );
+        sleep(2);
         $results = wp_handle_sideload($file, $overrides); // Move the temporary file into the uploads directory
 
         if (!empty($results['error'])) {
@@ -90,11 +82,11 @@ function bg_image_upload($url, $productTitle)
 $yaraMainProduct = array("ID" => 0, "title" => "none");
 $yaraMainWooProduct;
 $yaraVariationWooProduct = 0;
+$yaraTotalProducts = 0;
 
 
-function pricode_create_product($data)
+function yara_create_product($data)
 {
-
     $product = new WC_Product_Variable();
     $product->set_description($data['post_content']);
     $product->set_name($data['post_title']);
@@ -106,17 +98,8 @@ function pricode_create_product($data)
     return $product;
 }
 
-/**
- * Create Product Attributes 
- * @param  string $name    Attribute name
- * @param  array $options Options values
- * @return Object          WC_Product_Attribute 
- */
-function pricode_create_attributes($name, $options)
+function yara_create_attributes($name, $options)
 {
-
-
-
     $attribute = new WC_Product_Attribute();
     $attribute->set_id(0);
     $attribute->set_name($name);
@@ -126,13 +109,7 @@ function pricode_create_attributes($name, $options)
     return $attribute;
 }
 
-/**
- * [pricode_create_variations description]
- * @param  [type] $product_id [description]
- * @param  [type] $values     [description]
- * @return [type]             [description]
- */
-function pricode_create_variations($product_id, $values, $data)
+function yara_create_variations($product_id, $values, $data)
 {
     $variation = new WC_Product_Variation();
     $variation->set_parent_id($product_id);
@@ -145,78 +122,33 @@ function pricode_create_variations($product_id, $values, $data)
     $variation->save();
     $product = wc_get_product($product_id);
     $product->save();
+    return $variation;
 }
-
-
-
-
-
-
-
-
-
-
 
 function yara_get_data()
 {
 
-    global $yaraMainProduct, $yaraMainWooProduct, $yaraVariationWooProduct;
-
-
-
+    global $yaraMainProduct, $yaraMainWooProduct, $yaraVariationWooProduct, $yaraTotalProducts;
 
 
     $api_url = 'https://dummyjson.com/products';   // Source URL  
     $json_data = file_get_contents($api_url); // Read JSON file    
     $response_data = json_decode($json_data); // Decode JSON data into PHP array   
     $products_data = $response_data->products; // All user data products in 'data' object    
-    $products_data = array_slice($products_data, 0, 3); // FOR DEBUG !!! Cut long data into small & select only first few records
+    //$products_data = array_slice($products_data, 0, 3); // FOR DEBUG !!! Cut long data into small & select only first few records
 
-    // Print data if need to debug
-    //echo "<pre>";
-    //print_r($products_data);
-    //echo "</pre>";
-
-    echo "<pre>";
-    // Traverse array and display data
-    $atts = [];
-    $names = [];
+    $atts = $names = $images = $prices = [];
     foreach ($products_data as $index => $product) {
-        //print_r($product);
         $mainProduct = 0;
         if ($index % 3 == 0) {
             $mainProduct = 1;
         }
-
-        echo "yaraMainProduct ID: " . $yaraMainProduct['ID'];
-        echo "<br />";
-        echo "Count: " . $index . " mainProduct: " . $mainProduct;
-        echo "<br />";
-        echo "title: " . $product->title;
-        echo "<br />";
-        echo "description: " . $product->description;
-        echo "<br />";
-        echo "category: " . $product->category;
-        echo "<br />";
-        echo "price: " . $product->price;
-        echo "<br />";
-        echo "image 1: " . $product->images[0];
-        echo "<br />";
-
-
-        //echo "----------------------------------------------------------------";
-        //echo "<br /><br />";
-
-
         $productCategory = category_exists($product->category);
         $names[] = $product->title;
-
-        echo "category exist ?: " . $productCategory;
-        echo "<br />";
+        $images[] = $product->images[0];
+        $prices[] = $product->price;
 
         if (!$productCategory) {
-            echo "missing" . $productCategory;
-            echo "<br />";
             //BUG - 1 //The category needs more final touch !!!! Not Complete yet !!!
             wp_insert_term($product->category, 'product_cat', array(
                 'description' => $product->category
@@ -225,147 +157,76 @@ function yara_get_data()
             add_term_meta($productCategory, 'display_type', 'products');
         }
 
-
-        $mypostExist = post_exists($product->title);
         //BUG - 1 // post_category WOO products do not use default categories !!!
         if ($mainProduct) {
             $yara_post_type = 'product';
             $yara_post_parent = 0;
+            $my_yara_product_post = array(
+                'post_title'    => $product->title,
+                'post_content'  =>  $product->description,
+                'post_status'   => 'publish',
+                'post_type'   => $yara_post_type,
+                'post_parent'   => $yara_post_parent,
+                'post_category' => array($productCategory),
+                'post_price' => $product->price,
+                'post_author'   => 1
+            );
+
+            $mypostExist = post_exists($product->title);
+            if ($mypostExist == NULL) {
+                $yaraMainWooProduct = yara_create_product($my_yara_product_post);
+                $myNewPost = $yaraMainWooProduct->get_id();
+                //FIX - 1 // - scrap/send more data Alternative Text,Caption,Description//
+                if ($product->images[0]) {
+                    //FIX - 2 // - check if already Exist !!! //
+                    //FIX - 3 // - Set Some !!!DELAY!!! (write in loop) //
+                    $myProductImageId = bg_image_upload($product->images[0], $product->title);
+                    if (!add_post_meta($myNewPost, '_thumbnail_id', $myProductImageId, true)) {
+                        update_post_meta($myNewPost, '_thumbnail_id', $myProductImageId);
+                    }
+                }
+                $yaraTotalProducts++;
+            }
         } else {
             $yara_post_type = 'product_variation';
             $yara_post_parent = $yaraMainProduct['ID'];
 
             //create variations
             if ($yaraVariationWooProduct == 0) {
-
-
-                //$atts[] = pricode_create_attributes('color', ['red', 'green']);
                 $yaraVariationWooProduct++;
-                //Adding attributes to the created product
-
-
-
-
             } else if ($yaraVariationWooProduct == 1) {
+                
+                if ($yaraMainWooProduct) {
 
-                //Creating Attributes 
-                /*
-               
-                    $atts = [];
-                    $atts[] = pricode_create_attributes('col_or', ['red cato', 'green mnogo green po4ti 5']);
-                   
-                    //Adding attributes to the created product
+                    // BUG 2 function need lowercase string without whitespaces .. name can be updated after options was created by ID
+                    $anOption = strtolower(str_replace(array( '\'', '"', ',' , ';', '<', '>', '.', ' ' ), '_',  $names[0]));
+                    $atts[] = yara_create_attributes($anOption, $names);
+
                     $yaraMainWooProduct->set_attributes($atts);
                     $yaraMainWooProduct->save();
+                    foreach ($names as $index => $name) {
 
-                    //Setting data (following Alexander's rec
-                    $data = new stdClass();
-                    $data->sku = 'sku-123';
-                    $data->price = '10';
-                    //Create variations
-                    pricode_create_variations($yaraMainWooProduct->get_id(), ['col_or' => 'red cato'], $data);
-
-                    $dataB = new stdClass();
-                    $dataB->sku = 'sku-1235';
-                    $dataB->price = '15';
-                    pricode_create_variations($yaraMainWooProduct->get_id(), ['col_or' => 'green mnogo green po4ti 5',], $dataB);
-                  */
-
-
-
-                $anOption = strtolower(str_replace(' ', '_', $names[0]));
-
-
-                $atts[] = pricode_create_attributes($anOption, $names);
-
-                $yaraMainWooProduct->set_attributes($atts);
-                $yaraMainWooProduct->save();
-
-
-                echo "<pre>";
-                print_r($yaraMainWooProduct);
-                echo "</pre>";
-
-               
-
-
-                $options_data = new stdClass();
-                $options_data->sku = $names[0];
-                $options_data->price = '10';
-                //Create variations
-                pricode_create_variations($yaraMainWooProduct->get_id(), [$anOption => $names[0]], $options_data);
-
-                $options_data = new stdClass();
-                $options_data->sku = $names[1];
-                $options_data->price = '15';
-                //Create variations
-                pricode_create_variations($yaraMainWooProduct->get_id(), [$anOption => $names[1]], $options_data);
-
-                $options_data = new stdClass();
-                $options_data->sku = $names[2];
-                $options_data->price = '20';
-                //Create variations
-                pricode_create_variations($yaraMainWooProduct->get_id(), [$anOption => $names[2]], $options_data);
-
-
-
-
+                        $options_data = new stdClass();
+                        $options_data->sku = $name;
+                        $options_data->price = $prices[$index];
+                        //Create variations
+                        $yaraNewVariation = yara_create_variations($yaraMainWooProduct->get_id(), [$anOption => $name], $options_data);
+                        if ($images[$index]) {
+                            $myProductImageId = bg_image_upload($images[$index], $product->title);
+                            if (!add_post_meta($yaraNewVariation->get_id(), '_thumbnail_id', $myProductImageId, true)) {
+                                update_post_meta($yaraNewVariation->get_id(), '_thumbnail_id', $myProductImageId);
+                            }
+                        }
+                    }
+                }
                 $yaraVariationWooProduct = 0;
-                $atts = [];
-                $names = [];
+                $atts = $names = $images = $prices = [];
             }
         }
-
-
-        $my_yara_product_post = array(
-            'post_title'    => $product->title,
-            'post_content'  =>  $product->description,
-            'post_status'   => 'publish',
-            'post_type'   => $yara_post_type,
-            'post_parent'   => $yara_post_parent,
-            'post_category' => array($productCategory),
-            'post_price' => $product->price,
-            'post_author'   => 1
-        );
-
-        if ($mypostExist == NULL) {
-            //$myNewPost = wp_insert_post($my_yara_product_post);
-            if ($mainProduct) {
-                //$yaraMainProduct['ID'] = $myNewPost;
-                $yaraMainWooProduct = pricode_create_product($my_yara_product_post);
-
-
-                echo "PPR:::";
-            }
-            $myNewPost = post_exists($product->title);
-
-
-            // add_post_meta( int $post_id, string $meta_key, mixed $meta_value, bool $unique = false ): int|false
-            //FIX - 1 // - scrap/send more data Alternative Text,Caption,Description//
-            if ($product->images[0]) {
-
-
-
-
-                //FIX - 2 // - check if already Exist !!! //
-                //FIX - 3 // - Set Some !!!DELAY!!! (write in loop) //
-                $myProductImageId = bg_image_upload($product->images[0], $product->title);
-                //print_r($myProductImageId);
-                //echo "<br />";
-                if (!add_post_meta($myNewPost, '_thumbnail_id', $myProductImageId, true)) {
-                    update_post_meta($myNewPost, '_thumbnail_id', $myProductImageId);
-                }
-            }
-
-            print_r($myNewPost);
-            echo "<br />";
-            print_r($productCategory);
-            echo "<br />";
-
-            //BUG - 1 // post_category WOO products do not use default categories !!!
-            // do_action('add_term_relationship', $myNewPost, $productCategory, 0);
-        } 
-
     }
-    echo "</pre>";
+    if($yaraTotalProducts > 0) {
+    echo "<h3> " , $yaraTotalProducts , " Product/s was successfully created</h3>";
+    }else {
+        echo "<h3>Nothing to update</h3>";
+    }
 }
